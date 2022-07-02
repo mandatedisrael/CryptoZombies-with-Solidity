3029a142514828cff096ed0dd7ece8743b310a4e
Dami <mandatedisrael@gmail.com>
2022-07-02 14:15:51 +0100

    Update Zombie feeding level3

--- Source Code/zombiefeeding.sol
+++ Source Code/zombiefeeding.sol
@@ -1,4 +1,4 @@
-pragma solidity >=0.5.0 <0.6.0;
+pragma solidity ^0.4.25;
 
 import "./zombiefactory.sol";
 
@@ -25,26 +25,15 @@ contract ZombieFeeding is ZombieFactory {
     kittyContract = KittyInterface(_address);
   }
 
-  function _triggerCooldown(Zombie storage _zombie) internal {
-    _zombie.readyTime = uint32(now + cooldownTime);
-  }
-
-  function _isReady(Zombie storage _zombie) internal view returns (bool) {
-      return (_zombie.readyTime <= now);
-  }
-
-  // 1. Make this function internal
-  function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) public {
+  function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) public {
     require(msg.sender == zombieToOwner[_zombieId]);
     Zombie storage myZombie = zombies[_zombieId];
-    // 2. Add a check for `_isReady` here
     _targetDna = _targetDna % dnaModulus;
     uint newDna = (myZombie.dna + _targetDna) / 2;
     if (keccak256(abi.encodePacked(_species)) == keccak256(abi.encodePacked("kitty"))) {
       newDna = newDna - newDna % 100 + 99;
     }
     _createZombie("NoName", newDna);
-    // 3. Call `_triggerCooldown`
   }
 
   function feedOnKitty(uint _zombieId, uint _kittyId) public {
